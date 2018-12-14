7a75da71117b0e01d81377fa55e69709857da57e
erosenthal <eric.rosenthal@viasat.com>
2018-12-14 10:23:34 -0500

    testing the custom field for phone number

--- packages/rocketchat-contacts/server/startup.js
+++ packages/rocketchat-contacts/server/startup.js
@@ -1,5 +1,5 @@
 RocketChat.settings.addGroup('Contacts', function() {
-	this.add('Contacts_Phone_Custom_Field_Name', '', {
+	this.add('Contacts_Phone_Custom_Field_Name', 'services,viasatsso,telephoneNumber', {
 		type: 'string',
 		public: true,
 		i18nDescription: 'Contacts_Phone_Custom_Field_Name_Description',

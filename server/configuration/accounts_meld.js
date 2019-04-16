bcdf627f5bd002c63006518d289733dfc6c1f15b
erosenthal <eric.rosenthal@viasat.com>
2019-04-16 17:47:25 -0400

    add name to user if it does not exist.....  backwards compatibility

--- server/configuration/accounts_meld.js
+++ server/configuration/accounts_meld.js
@@ -51,6 +51,9 @@ Accounts.updateOrCreateUserFromExternalService = function(serviceName, serviceDa
 				RocketChat.models.Users.setServiceId(user._id, serviceName, serviceData.id);
 				RocketChat.models.Users.setEmail(user._id, serviceData.email);
 				RocketChat.models.Users.setEmailVerified(user._id, serviceData.email);
+				if (user.name == null) {
+					RocketChat.models.Users.setName(user._id, serviceData.userid);
+				}
 			}
 		}
 	}

355183e9c3361c73b1f00cb4b17e2d5a300ddb7c
shubhsherl <shubham.singh094@yahoo.com>
2019-07-12 16:23:31 +0530

    fix import error

--- client/importPackages.js
+++ client/importPackages.js
@@ -110,6 +110,4 @@ import '../app/ui-cached-collection';
 import '../app/action-links';
 import '../app/reactions/client';
 import '../app/livechat/client';
-import '../app/newsfeed/client';
-import '../app/follow/client';
 import '../app/articles/client';

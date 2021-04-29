e7fbd112fb6d8a35056ce041af4f59001c9b0d08
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2021-04-29 12:19:54 +0530

    Fix Department Field

--- client/views/omnichannel/directory/chats/contextualBar/DepartmentField.js
+++ client/views/omnichannel/directory/chats/contextualBar/DepartmentField.js
@@ -14,13 +14,11 @@ const DepartmentField = ({ departmentId }) => {
 	if (state === AsyncStatePhase.LOADING) {
 		return <FormSkeleton />;
 	}
-	const {
-		department: { name },
-	} = data || { department: {} };
+	const { department } = data || { department: {} };
 	return (
 		<Field>
 			<Label>{t('Department')}</Label>
-			<Info>{name}</Info>
+			<Info>{department && department.name}</Info>
 		</Field>
 	);
 };

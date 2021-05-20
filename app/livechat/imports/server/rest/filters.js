05ddfd7e430f35921e90c84e13d299968aa86a86
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2021-05-20 23:31:57 +0530

    Add livechat/filter post API Endpoint

--- app/livechat/imports/server/rest/filters.js
+++ app/livechat/imports/server/rest/filters.js
@@ -1,7 +1,30 @@
+import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
 import { API } from '../../../../api/server';
 import { findFilters, findFilterById } from '../../../server/api/lib/filters';
+
+API.v1.addRoute('livechat/filter', { authRequired: true }, {
+	post() {
+		try {
+			check(this.bodyParams, {
+				name: String,
+				description: String,
+				enabled: Boolean,
+				regex: String,
+				slug: String,
+			});
+
+			API.v1.success(
+				Meteor.runAsUser(this.userId,
+					() => Meteor.call('livechat:saveFilter', this.bodyParams),
+				),
+			);
+		} catch (e) {
+			return API.v1.failure(e);
+		}
+	},
+});
 
 API.v1.addRoute('livechat/filters', { authRequired: true }, {
 	get() {

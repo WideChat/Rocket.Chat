ec3f17e2b71e0ad59c8fe639729571ab09f45e0e
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2021-07-15 21:45:56 +0530

    Chore: Remove unnecessary modals replacing to GenericModal

--- client/views/omnichannel/filters/FiltersRow.js
+++ client/views/omnichannel/filters/FiltersRow.js
@@ -2,7 +2,7 @@ import { Table, Icon, Button } from '@rocket.chat/fuselage';
 import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
 import React, { memo } from 'react';
 
-import DeleteWarningModal from '../../../components/DeleteWarningModal';
+import GenericModal from '../../../components/GenericModal';
 import { useSetModal } from '../../../contexts/ModalContext';
 import { useRoute } from '../../../contexts/RouterContext';
 import { useMethod } from '../../../contexts/ServerContext';
@@ -49,7 +49,14 @@ const FiltersRow = memo(function FiltersRow(props) {
 			setModal();
 		};
 
-		setModal(<DeleteWarningModal onDelete={onDeleteFilter} onCancel={() => setModal()} />);
+		setModal(
+			<GenericModal
+				variant='danger'
+				onConfirm={onDeleteFilter}
+				onCancel={() => setModal()}
+				confirmText={t('Delete')}
+			/>,
+		);
 	});
 
 	return (
